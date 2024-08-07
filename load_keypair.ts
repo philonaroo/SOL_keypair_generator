6882a877809ddfc34fd49290ce5c30443516f778
philonaroo <123151419+philonaroo@users.noreply.github.com>
2024-08-07 22:40:02 +0300

    Add files via upload

---
+++ load_keypair.ts
@@ -0,0 +1,9 @@
+import "dotenv/config";
+import { Keypair } from "@solana/web3.js";
+
+const secretKey = Uint8Array.from(JSON.parse(process.env["SECRET_KEY"]))
+const keypair = Keypair.fromSecretKey(secretKey);
+
+console.log(
+  `✅ Loaded keypair securely! Public key is: ${keypair.publicKey.toBase58()}`
+);
